4f4be742792ba84a0188e538fe1b7b3e155e67e0
root <mgiraudo@ies21.edu.ar>
2024-03-27 22:19:35 -0300

    rectangulo picture box

--- ProyEjemplo/Form1.cs
+++ ProyEjemplo/Form1.cs
@@ -16,5 +16,19 @@ namespace ProyEjemplo
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics papel;
+            papel = pictureBox1.CreateGraphics();
+            Pen lápiz = new Pen(Color.Black);
+            papel.DrawRectangle(lápiz, 10, 10, 100, 50);
+            papel.DrawRectangle(lápiz, 10, 75, 100, 100);
+        }
     }
 }
--- ProyEjemplo/FormFirma.Designer.cs
+++ ProyEjemplo/FormFirma.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dibujar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(248, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 291);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormFirma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(648, 508);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "FormFirma";
             this.Text = "FormFirma";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FormFirma_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
